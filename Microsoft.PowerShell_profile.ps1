c00e47f783f68e3fb0c63ec5d85539e9e53a850b
Sebastian Nicolas <snicolas@analytia.pe>
2025-11-01 20:58:07 -0500

    initial

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,24 @@
+function y {
+    $tmp = (New-TemporaryFile).FullName
+    yazi $args --cwd-file="$tmp"
+    $cwd = Get-Content -Path $tmp -Encoding UTF8
+    if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+        Set-Location -LiteralPath (Resolve-Path -LiteralPath $cwd).Path
+    }
+    Remove-Item -Path $tmp
+}
+
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
+Invoke-Expression (&starship init powershell)
+
+function sdk {
+    param(
+        [string]$version
+    )
+    $packageName = "corretto" + $version + "-jdk"
+    scoop reset $packageName
+}
+
+Set-Alias -Name c -Value Clear-Host
+Set-Alias -Name vi -Value nvim
+Set-Alias -Name lg -Value lazygit
